1601bad67a371924e81c06c2ef6e273235707a5a
Nadhif Lazuardi Aulia Rahim <berkasnadhif@gmail.com>
2025-02-25 13:13:43 +0700

    refactor project structure

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e47689a9619b789332ceaa55ef8fb9420ecd84ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6891c3e60f4acb52b8a7c2de34c559357c800822")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
