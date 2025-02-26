27ca6e9cf2f4e2b4a968d4894796fe78532ef930
Nadhif Lazuardi Aulia Rahim <berkasnadhif@gmail.com>
2025-02-26 09:41:54 +0700

    add number guessing game project

---
+++ Day 3/NumberGuessingGame/Program.cs
@@ -0,0 +1,35 @@
+﻿Random numberGenerator = new();
+
+int randomNumber = numberGenerator.Next(101);
+string input;
+string message = "";
+int userGuess = 0;
+
+while (true)
+{
+  Console.Clear();
+  Console.WriteLine("Choose a number between 1-100");
+
+  if (userGuess != 0)
+  {
+    Console.WriteLine($"Your last guess: {userGuess}. {message}");
+  }
+
+  Console.Write("Input: ");
+  input = Console.ReadLine() ?? "0";
+  userGuess = Convert.ToInt32(input);
+
+  if (userGuess == randomNumber)
+  {
+    Console.WriteLine("You won!");
+    break;
+  }
+  else if (userGuess > randomNumber)
+  {
+    message = "Try lower!";
+  }
+  else
+  {
+    message = "Try higher!";
+  }
+}
