ef09ad39ff18683527f28c1de39a6a698acd78c7
Nadhif Lazuardi Aulia Rahim <berkasnadhif@gmail.com>
2025-02-28 14:37:30 +0700

    learn nullable value types

---
+++ Day 5/NullableValueTypes/Program.cs
@@ -0,0 +1,48 @@
+﻿// class CheckNullability
+// {
+//   static void Main()
+//   {
+//     int? i = null;
+//     Console.WriteLine(i == null);
+//   }
+// }
+
+// class HasValue
+// {
+//   static void Main()
+//   {
+//     int? x = 20;
+
+//     if (x.HasValue)
+//     {
+//       Console.WriteLine(x);
+//       Console.WriteLine(x.Value);
+//       Console.WriteLine(x == x.Value);
+//     } else {
+//       Console.WriteLine("x is null");
+//     }
+//   }
+// }
+
+// class NullCoalescing
+// {
+//   static void Main()
+//   {
+//     int? x = null;
+//     int y = x ?? 5;
+//     Console.WriteLine(y);
+
+//     int? a = null, b = 1, c = 2;
+//     Console.WriteLine(a ?? b ?? c);
+//   }
+// }
+
+class NullConversion
+{
+  static void Main()
+  {
+    object o = "string";
+    int? x = o as int?;
+    Console.WriteLine(x.HasValue);
+  }
+}
