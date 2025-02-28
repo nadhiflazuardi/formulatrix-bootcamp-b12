2e76033138a23deba60c642716f14aa37c2baaa1
Nadhif Lazuardi Aulia Rahim <berkasnadhif@gmail.com>
2025-02-28 10:55:50 +0700

    learn delegates

---
+++ Day 5/Delegates/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+  public delegate void Callback(string message);
+
+  public static void DelegateMethod(string message)
+  {
+    Console.WriteLine(message);
+  }
+
+  public static void DelegateMethod2(string message) {
+    Console.WriteLine("Success!");
+  }
+
+  public static void MethodWithCallback(int param1, int param2, Callback callback)
+  {
+    callback("The number is: " + (param1 + param2).ToString());
+  }
+
+  static void Main()
+  {
+    Callback handler = DelegateMethod;
+    handler += DelegateMethod2;
+    MethodWithCallback(1, 2, handler);
+  }
+}
