e3c5dcf7c7772fab870ec66ff28cc3859e0d826f
Nadhif Lazuardi Aulia Rahim <berkasnadhif@gmail.com>
2025-02-28 14:54:58 +0700

    learn iterator

---
+++ Day 5/Iterator/Program.cs
@@ -0,0 +1,22 @@
+﻿class FibonacciSequence
+{
+  static void Main()
+  {
+    IEnumerable<int> Fibs(int reqFib)
+    {
+      int prevFib = 0, curFib = 1;
+      for (int i = 0; i < reqFib; i++)
+      {
+        yield return prevFib;
+        int newFib = prevFib + curFib;
+        prevFib = curFib;
+        curFib = newFib;
+      }
+    }
+
+    foreach (int fib in Fibs(6))
+    {
+      Console.WriteLine(fib + " ");
+    }
+  }
+}
