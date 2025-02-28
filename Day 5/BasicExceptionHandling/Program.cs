c839851e248843c7344685e8d8b23297ddcc6160
Nadhif Lazuardi Aulia Rahim <berkasnadhif@gmail.com>
2025-02-28 14:20:37 +0700

    learn try statement

---
+++ Day 5/BasicExceptionHandling/Program.cs
@@ -0,0 +1,16 @@
+﻿int Calc(int x)
+{
+  return 10 / x;
+}
+
+try {
+  Console.WriteLine("Trying to divide by zero.");
+  int result = Calc(0);
+  Console.WriteLine($"Result: {result}");
+} catch (DivideByZeroException ex) {
+  Console.WriteLine($"Error caught: {ex.Message}");
+} finally {
+  Console.WriteLine("This code block will always execute.");
+}
+
+Console.WriteLine("End of program.");
