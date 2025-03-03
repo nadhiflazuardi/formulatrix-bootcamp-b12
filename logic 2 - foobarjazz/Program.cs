c0f8b6a813d470f48621266b3335136333f082b0
Nadhif Lazuardi Aulia Rahim <berkasnadhif@gmail.com>
2025-03-03 08:19:48 +0700

    add 2nd logic exercise - foobarjazz

---
+++ logic 2 - foobarjazz/Program.cs
@@ -0,0 +1,46 @@
+﻿class Program
+{
+  static void Foobarjazz(int n)
+  {
+    for (int i = 1; i <= n; i++)
+    {
+      if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0)
+      {
+        Console.WriteLine("foobarjazz");
+      }
+      else if (i % 3 == 0 && i % 5 == 0)
+      {
+        Console.WriteLine("foobar");
+      }
+      else if (i % 3 == 0 && i % 7 == 0)
+      {
+        Console.WriteLine("foojazz");
+      }
+      else if (i % 5 == 0 && i % 7 == 0)
+      {
+        Console.WriteLine("barjazz");
+      }
+      else if (i % 3 == 0)
+      {
+        Console.WriteLine("foo");
+      }
+      else if (i % 5 == 0)
+      {
+        Console.WriteLine("bar");
+      }
+      else if (i % 7 == 0)
+      {
+        Console.WriteLine("jazz");
+      }
+      else
+      {
+        Console.WriteLine(i);
+      }
+    }
+  }
+
+  static void Main()
+  {
+    Foobarjazz(105);
+  }
+}
