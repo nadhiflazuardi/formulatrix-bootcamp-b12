5a9d637d9faf0db227c0910f86a39ead10aea839
Nadhif Lazuardi Aulia Rahim <berkasnadhif@gmail.com>
2025-03-04 11:58:24 +0700

    learn enum

---
+++ Day 7/Enum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+
+enum PointValues
+{
+  Miss = 0,
+  Bronze = 10,
+  Silver = 25,
+  Gold = 50,
+}
+
+class Program
+{
+  static void Main()
+  {
+    PointValues[] hits = [
+      PointValues.Gold,
+      PointValues.Silver,
+      PointValues.Miss,
+      PointValues.Silver,
+      PointValues.Bronze
+    ];
+
+    int totalScore = 0;
+    foreach (PointValues hit in hits) {
+      totalScore += (int) hit;
+    }
+
+    Console.WriteLine($"Total score: {totalScore}");
+  }
+}
