3bb8d8ac3dc559f6dd408568c54aa30b7ba6c73e
Nadhif Lazuardi Aulia Rahim <berkasnadhif@gmail.com>
2025-03-04 11:07:14 +0700

    learn formatting and parsing

---
+++ Day 7/FormattingAndParsing/Program.cs
@@ -0,0 +1,28 @@
+﻿class Formatting
+{
+  public static void Format()
+  {
+    Console.WriteLine(true.ToString());
+  }
+}
+
+class Parsing
+{
+  public static void Parse()
+  {
+    int integerr;
+    Console.WriteLine();
+    Console.WriteLine(int.Parse("24"));
+    Console.WriteLine(int.TryParse("24", out integerr));
+    Console.WriteLine(integerr);
+  }
+}
+
+class Program
+{
+  static void Main()
+  {
+    Formatting.Format();
+    Parsing.Parse();
+  }
+}
